7a3a4fada8505ad5ce516da815c94259665bc060
ahmedNewiry <ahmedabadalfttah@outlook.com>
2024-08-29 23:07:18 +0000

    fix

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -17,7 +17,6 @@ def filter_datum(fields: List[str], redaction: str, message: str,
 
 class RedactingFormatter(logging.Formatter):
     """Redacting Formatter class"""
-
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
