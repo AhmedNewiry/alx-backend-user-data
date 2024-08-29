60a240c418c855dfe3c1532849983eb006e62050
ahmedNewiry <ahmedabadalfttah@outlook.com>
2024-08-29 23:40:21 +0000

    get_logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -39,9 +39,7 @@ def get_logger() -> logging.Logger:
     logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
     logger.propagate = False
-
     handler = logging.StreamHandler()
     handler.setFormatter(RedactingFormatter(fields=PII_FIELDS))
     logger.addHandler(handler)
-
     return logger
