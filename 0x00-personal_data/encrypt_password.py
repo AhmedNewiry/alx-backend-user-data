d1e88b357ad3628fd9618f2a2707ebde95ecc843
ahmedNewiry <ahmedabadalfttah@outlook.com>
2024-08-30 00:27:12 +0000

    fix

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -13,3 +13,8 @@ def hash_password(password: str) -> bytes:
     # Hash the password
     hashed = bcrypt.hashpw(password.encode(), salt)
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Check if the provided password matches the hashed password."""
+    return bcrypt.checkpw(password.encode(), hashed_password)
