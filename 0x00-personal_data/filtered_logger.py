4779d11c633b671de4535536fe5342e03599738f
ahmedNewiry <ahmedabadalfttah@outlook.com>
2024-08-30 00:13:26 +0000

    get_sql

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -54,18 +54,14 @@ def get_logger() -> logging.Logger:
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
     """Connects to the MySQL database and returns the connection object."""
-    # Retrieve database credentials from environment variables
     username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
     password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
     host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
     db_name = os.getenv("PERSONAL_DATA_DB_NAME")
-
-    # Create a connection to the database
     connection = mysql.connector.connect(
         user=username,
         password=password,
         host=host,
         database=db_name
     )
-
     return connection
