8731ac197290f01ffc605cb28d75b677fa555f3f
ahmedNewiry <ahmedabdalfttah@outlook.com>
2024-09-07 17:07:16 -0700

    fix

--- 0x02-Session_authentication/api/v1/views/__init__.py
+++ 0x02-Session_authentication/api/v1/views/__init__.py
@@ -9,8 +9,6 @@ app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 from api.v1.views.users import *
 from api.v1.views.index import *
 from .session_auth import session_auth_bp
-
-# Register the blueprint
-app.register_blueprint(session_auth_bp)
+from api.v1.views.session_auth import *
 
 User.load_from_file()
