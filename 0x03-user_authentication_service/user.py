f2c91eaf213b1467583c972c727f589bd75fec2c
ahmedNewiry <ahmedabdalfttah@outlook.com>
2024-09-10 18:10:13 +0000

    pep8

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -11,12 +11,11 @@ Base = declarative_base()
 
 class User(Base):
     """User model representing the users table"""
-    
+
     __tablename__ = 'users'
-    
+
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
-
