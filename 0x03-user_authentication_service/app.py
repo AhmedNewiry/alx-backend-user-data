0c5caee15a0b3990097cda1eff799c9388e0af89
ahmedNewiry <ahmedabdalfttah@outlook.com>
2024-09-10 22:40:22 +0000

    login

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,7 +3,7 @@
 Flask app module to handle user registration.
 """
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort, make_response
 from auth import Auth
 
 # Instantiate the Auth object
@@ -37,6 +37,27 @@ def users():
         return jsonify({"email": user.email, "message": "user created"}), 201
     except ValueError as e:
         return jsonify({"message": str(e)}), 400
+
+
+@app.route('/sessions', methods=['POST'])
+def login():
+    """Handle POST requests to /sessions for user login"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not email or not password:
+        abort(400, description="Missing email or password")
+
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        response = jsonify({
+            "email": email,
+            "message": "logged in"
+        })
+        response.set_cookie('session_id', session_id)
+        return response
+    else:
+        abort(401, description="Invalid login credentials")
 
 
 # Run the Flask app
